2b87af5bd4ae4a7851a417e086243f1b15dd999d
NetAcad DEVASC <changeme@example.com>
2025-12-16 19:38:34 +0000

    sample_app.py y sample-app.sh.

--- sample-app/sample_app.py
+++ sample-app/sample_app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import sys 
 
 sample = Flask(__name__)
 
@@ -10,4 +11,17 @@ def main():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    sample.run(host="0.0.0.0", port=8080)
+    
+    
+    port_to_run = 8080 
+    
+   
+    if len(sys.argv) > 1:
+        try:
+            port_to_run = int(sys.argv[1])
+        except ValueError:
+            
+            pass 
+            
+    
+    sample.run(host="0.0.0.0", port=port_to_run, debug=True)
